b45f7a22de5d400c19e93d0508d8edd9a5a62573
ozdemirsalih98-ui <ozdemirsalih98@gmail.com>
2026-04-19 02:44:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'kasa-defteri-v1';
+const FILES = ['./kasa-defteri.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
